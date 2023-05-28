830fa5c005aecc8d14ec0ec2b9e721800671dddf
Hunter Zepp <122405685+HunterZepp24@users.noreply.github.com>
2023-05-27 20:37:20 -0700

    Added Styles and Fonts

--- js/scripts.js
+++ js/scripts.js
@@ -6,8 +6,8 @@ const url = "https://assets.codepen.io/16425/spring23web3.json";
 
 // Get data
 fetch(url)
-  .then( response  => response.json())
-  .then( data  => {
+  .then(response => response.json())
+  .then(data => {
     // check-check: get one image
     // Note: Webflow returns data in array called `items`
     console.log(data);
@@ -23,18 +23,31 @@ fetch(url)
     const roster = document.querySelector(".roster");
 
     // loop through data
-    data.forEach( student => {
-      
+    data.forEach(student => {
+
       // template
       const template = `
-          <figure>
-            <figcaption>${student.Name}</figcaption>
-            <p>${student.Emoji}</p>
-            <img src="${student.Image}" alt="${student.Name}">
+          <figure class="card" id="${student.Name}">
+            <div class="card-inner">
+              <img src="${student.Image}" alt="${student.Name}">
+              <figcaption>
+                <h1>${student.Name}</h1>
+                <blockquote>${student.Quote}</blockquote>
+                <p>${student.Superpower}</p>
+                <p><span>${student.Emoji}</span></p>
+              </figcaption>
+            </div>
           </figure>
        `;
 
       // insert EACH `student` record into container
       roster.insertAdjacentHTML("afterbegin", template);
+
+      // change the boxshadow color of each to student.Color by id
+      document.getElementById(student.Name).style.boxShadow = `0 0 20px 20px ${student.Color}, 0 0 20px 20px ${student.Color} inset`;
+
+      // change the color of each h1 to student.Color
+      document.getElementById(student.Name).querySelector("h1").style.color = student.Color;
+
     });
   });
